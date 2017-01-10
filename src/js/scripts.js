64210c1fdccd43fc82bffed1aa247346e1166e7e
Luca Develo <luca.develo@gmail.com>
2017-01-10 18:44:09 +0100

    angular-bootstrap

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,17 +5,9 @@
   $(window).load(function() {
     $(".loader").delay(800).fadeOut("slow");
   })
-  $window = $(window);
-  if( $window .width() > 800){
   
-    $('.parallax-scroll1').parallax("50%", 0.5);
-    $('.parallax-scroll3').parallax("50%", 0.5);
-    $('.parallax-scroll4').parallax("50%", 0.5);
-    $('.parallax-scroll5').parallax("50%", 0.5);
-  }
-
   $(function () {
-    // FastShell
+
   });
 
 })(jQuery, window, document);
